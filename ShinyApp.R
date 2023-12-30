c1a2635da4a74389bf53946ad05c710696b6d286
lino-zurmuehl <66783355+lino-zurmuehl@users.noreply.github.com>
2023-12-30 20:14:20 +0100

    Added Icon
    Icon added and first tries in size of clusters

--- ShinyApp.R
+++ ShinyApp.R
@@ -2,34 +2,52 @@ library(shiny)
 library(tidyverse)
 library(leaflet)
 library(fontawesome)
+library(htmlwidgets)
 
 mm_df <- read.csv("Global Missing Migrants Dataset 2.csv") %>% 
   separate(Coordinates, into = c("lat", "lon"), sep=",") %>% 
   mutate(lat = as.numeric(lat),
          lon = as.numeric(lon))
 
-icons <- makeAwesomeIcon(
-  icon = 'person-circle-question',
-  iconColor = 'black',
-  library = 'fa'
-)
+icons <- makeIcon(iconUrl = "person-circle-question-solid.png",
+  iconWidth = 18, iconHeight = 18)
+
 
 worldmap <- leaflet(mm_df) %>% 
   addTiles() %>%
-  addAwesomeMarkers(~lon, ~lat, icon = icons, 
-  clusterOptions = markerClusterOptions(iconCreateFunction=JS("function (cluster) {    
-    var childCount = cluster.getChildCount();  
-    if (childCount < 100) {  
-      c = 'rgba(255, 215, 0, 0.8);'
-    } else if (childCount < 500){
-      c = 'rgba(255, 69, 0, 0.8);'
-    }
-    else if (childCount < 1000) {  
-      c = 'rgba(255, 0, 0, 0.8);'  
-    } else { 
-      c = 'rgba(203, 0, 199, 0.8);'  
-    }    
-    return new L.DivIcon({ html: '<div style=\"background-color:'+c+'\"><span>' + childCount + '</span></div>', className: 'marker-cluster', iconSize: new L.Point(40, 40) });
-    }")))
+  addMarkers(
+    ~lon, ~lat, 
+    icon = icons, 
+    clusterOptions = markerClusterOptions(maxClusterSize = 10,
+      iconCreateFunction = JS("
+        function (cluster) {    
+          var childCount = cluster.getChildCount();
+          var size = 40;
+          var c = 'rgba(255, 215, 0, 0.8)'; 
+          
+          if (childCount < 100) {  
+            c = 'rgba(255, 215, 0, 0.8)';
+            size = 10;
+          } else if (childCount < 500){
+            c = 'rgba(255, 69, 0, 0.8)';
+            size = 20;
+          } else if (childCount < 1000) {  
+            c = 'rgba(255, 0, 0, 0.8)';
+            size = 30;
+          } else { 
+            c = 'rgba(203, 0, 199, 0.8)';
+            size = 40;
+          }    
+          
+          return new L.DivIcon({ 
+            html: '<div style=\"background-color:' + c + '\"><span>' + childCount + '</span></div>', 
+            className: 'marker-cluster', 
+            iconSize: new L.Point(size, size) 
+          });
+        }
+      ")))
+
 
 worldmap  
+
+
