02cbe1a31e2f8e1cfa4269da53b2f2bb9dc90ace
lino-zurmuehl <66783355+lino-zurmuehl@users.noreply.github.com>
2024-01-14 22:56:24 +0100

    Gut App running
    Need to change output filter

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,53 +1,77 @@
 library(shiny)
 library(tidyverse)
 library(leaflet)
-library(fontawesome)
-library(htmlwidgets)
+library(htmltools)
 
+# Read the data
 mm_df <- read.csv("Global Missing Migrants Dataset 2.csv") %>% 
   separate(Coordinates, into = c("lat", "lon"), sep=",") %>% 
   mutate(lat = as.numeric(lat),
          lon = as.numeric(lon))
 
+# Define icons and labels
 icons <- makeIcon(iconUrl = "person-circle-question-solid.png",
-  iconWidth = 18, iconHeight = 18)
+                  iconWidth = 18, iconHeight = 18)
 
+labels <- sprintf(
+  "<strong>Number of Deaths: %s</strong><br/>Cause of Death: %s<br/>Migration Route: %s",
+  mm_df$Number.of.Dead, mm_df$Cause.of.Death, mm_df$Migration.route) %>%
+  lapply(htmltools::HTML)
 
-worldmap <- leaflet(mm_df) %>% 
-  addTiles() %>%
-  addMarkers(
-    ~lon, ~lat, 
-    icon = icons, 
-    clusterOptions = markerClusterOptions(maxClusterSize = 10,
-      iconCreateFunction = JS("
-        function (cluster) {    
-          var childCount = cluster.getChildCount();
-          var size = 40;
-          var c = 'rgba(255, 215, 0, 0.8)'; 
-          
-          if (childCount < 100) {  
-            c = 'rgba(255, 215, 0, 0.8)';
-            size = 10;
-          } else if (childCount < 500){
-            c = 'rgba(255, 69, 0, 0.8)';
-            size = 20;
-          } else if (childCount < 1000) {  
-            c = 'rgba(255, 0, 0, 0.8)';
-            size = 30;
-          } else { 
-            c = 'rgba(203, 0, 199, 0.8)';
-            size = 40;
-          }    
-          
-          return new L.DivIcon({ 
-            html: '<div style=\"background-color:' + c + '\"><span>' + childCount + '</span></div>', 
-            className: 'marker-cluster', 
-            iconSize: new L.Point(size, size) 
-          });
-        }
-      ")))
+# Define the UI
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("category", "Select Geographical Grouping", choices = unique(mm_df$UNSD.Geographical.Grouping)),
+      selectInput("cause", "Select Cause of Death", choices = unique(mm_df$Cause.of.Death))
+    ),
+    mainPanel(leafletOutput("worldmap"))
+  )
+)
 
+# Define the server
+server <- function(input, output, session) {
+  output$worldmap <- renderLeaflet({
+    leaflet(mm_df) %>% 
+      addTiles() %>%
+      addMarkers(
+        ~lon, ~lat, 
+        icon = icons, 
+        label = labels,
+        clusterOptions = markerClusterOptions(maxClusterSize = 10,
+                                              iconCreateFunction = JS("
+                                                function (cluster) {    
+                                                  var childCount = cluster.getChildCount();
+                                                  var size = 20;
 
-worldmap  
+                                                  var c = 'rgba(255, 215, 0, 0.8)'; 
+                                                  
+                                                  if (childCount < 50) {  
+                                                    c = 'rgba(255, 165, 0, 0.7)';
+                                                    size = 10;
+                                                  } else if (childCount < 100){
+                                                    c = 'rgba(255, 140, 0, 0.7)';
+                                                    size = 20;
+                                                  } else if (childCount < 500) {  
+                                                    c = 'rgba(255, 69, 0, 0.7)';
+                                                    size = 30;
+                                                  } else if (childCount < 1000) {  
+                                                    c = 'rgba(255, 0, 0, 0.7)';
+                                                    size = 30;
+                                                  } else { 
+                                                    c = 'rgba(139, 0, 0, 0.7)';
+                                                    size = 40;
+                                                  }    
+                                                  
+                                                  return new L.DivIcon({ 
+                                                    html: '<div style=\"background-color:' + c + '\"><span>' + childCount + '</span></div>', 
+                                                    className: 'marker-cluster', 
+                                                    iconSize: new L.Point(size, size) 
+                                                  });
+                                                }
+                                              ")))
+  })
+}
 
-
+# Run the app
+shinyApp(ui, server)
