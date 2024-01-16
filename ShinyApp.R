9f519de57b63c62882181e3f44a4e8c73dc2b83f
lino-zurmuehl <66783355+lino-zurmuehl@users.noreply.github.com>
2024-01-16 10:05:40 +0100

    We are live!

--- ShinyApp.R
+++ ShinyApp.R
@@ -8,26 +8,28 @@ mm_df <- read.csv("Global Missing Migrants Dataset 2.csv") %>%
   separate(Coordinates, into = c("lat", "lon"), sep=",") %>% 
   mutate(lat = as.numeric(lat),
          lon = as.numeric(lon)) %>% 
+  drop_na(lat, lon) %>%
   filter(Incident.Type == "Incident") %>% 
-  drop_na()
+  mutate(Migration.route = case_when(
+    Migration.route == "" ~ "Uncategorized",
+    Migration.route == "Western Africa / Atlantic route to the Canary Islands" 
+                        ~ "Atlantic route to the Canary Islands",
+    TRUE ~ Migration.route))
 
 # Define icons and labels
 icons <- makeIcon(iconUrl = "person-circle-question-solid.png",
                   iconWidth = 18, iconHeight = 18)
 
-labels <- sprintf(
-  "<strong>Number of Deaths: %s</strong><br/>Cause of Death: %s<br/>Migration Route: %s",
-  mm_df$Number.of.Dead, mm_df$Cause.of.Death, mm_df$Migration.route) %>%
-  lapply(htmltools::HTML)
+
 
 # Define the UI
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("category", "Select Geographical Grouping", choices = c("None", unique(mm_df$UNSD.Geographical.Grouping))),
-      selectInput("month", "Select Reported Month", choices = c("None", unique(mm_df$Reported.Month))),
-      selectInput("year", "Select Year", choices = c("None", unique(mm_df$Incident.year)))
-    ),
+      selectInput("year", "Select Year", choices = c("None", unique(mm_df$Incident.year))),
+      selectInput("month", "Select Month of Year", choices = c("None", unique(mm_df$Reported.Month))),
+      plotOutput("incidents_plot")),
     mainPanel(leafletOutput("worldmap"))
   )
 )
@@ -37,13 +39,19 @@ server <- function(input, output, session) {
   filtered_data <- reactive({
     filter(
       mm_df,
-      if (input$month != "None") Reported.Month == input$month else TRUE,
       if (input$category != "None") UNSD.Geographical.Grouping == input$category else TRUE,
-      if (input$year != "None") Incident.year == input$year else TRUE
+      if (input$year != "None") Incident.year == as.numeric(input$year) else TRUE,
+      if (input$month != "None") Reported.Month == input$month else TRUE
     )
   })
   
+  
   output$worldmap <- renderLeaflet({
+    labels <- sprintf(
+      "<strong>Number of Deaths: %s</strong><br/>Cause of Death: %s<br/>Migration Route: %s",
+      filtered_data()$Number.of.Dead, filtered_data()$Cause.of.Death, filtered_data()$Migration.route) %>%
+      lapply(htmltools::HTML)
+    
     leaflet(filtered_data()) %>% 
       addTiles() %>%
       addMarkers(
@@ -82,6 +90,21 @@ server <- function(input, output, session) {
                                                   });
                                                 }
                                               ")))
+  })
+  
+  output$incidents_plot <- renderPlot({
+    incidents_per_mr <- filtered_data() %>%
+      group_by(Migration.route) %>%
+      summarise(Incidents = n())
+    
+    ggplot(incidents_per_mr, aes(x = Migration.route, y = Incidents, fill = Incidents)) +
+      geom_bar(stat = "identity") +
+      scale_fill_gradient(low = "red", high = "darkred")+
+      labs(title = "Incidents per Migration Route",
+           x = "Migration Routes",
+           y = "Number of Incidents (Log Scale)") +
+      scale_y_log10() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, ))
   })
 }
 
